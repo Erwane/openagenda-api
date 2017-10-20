02f39b7686434079b76acbd3fb709ffbd48d0cb8
Erwane Breton <erwane@phea.fr>
2017-10-20 12:46:21 +0200

    cache lib

--- src/Cache.php
+++ src/Cache.php
@@ -5,10 +5,29 @@ use Symfony\Component\Cache\Simple\FilesystemCache;
 
 class Cache
 {
+    /**
+     * read cache value
+     * @param  string $key cache key
+     * @return mixed
+     */
     public static function read($key)
     {
         $cache = new FilesystemCache();
 
         return $cache->get($key);
+    }
+
+    /**
+     * write cache value
+     * @param  string $key  cache key
+     * @param  mixed $value cache value
+     * @param  int $ttl     ttl cache lifetime
+     * @return
+     */
+    public static function write($key, $value, $ttl = null)
+    {
+        $cache = new FilesystemCache();
+
+        return $cache->set($key, $value, $ttl);
     }
 }
