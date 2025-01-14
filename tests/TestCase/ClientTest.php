bce8a44bdf0f6afab9a031ee29acefc41ea2eaef
Erwane <erwane@phea.fr>
2025-01-14 12:24:43 +0100

    @copyright

--- tests/TestCase/ClientTest.php
+++ tests/TestCase/ClientTest.php
@@ -1,6 +1,17 @@
 <?php
 declare(strict_types=1);
 
+/**
+ * OpenAgenda API client.
+ * Copyright (c) Erwane BRETON
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Erwane BRETON
+ * @see         https://github.com/Erwane/openagenda-api
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ */
 namespace OpenAgenda\Test\TestCase;
 
 use Exception;
--- tests/TestCase/Endpoint/AgendaTest.php
+++ tests/TestCase/Endpoint/AgendaTest.php
@@ -12,7 +12,6 @@ declare(strict_types=1);
  * @see         https://github.com/Erwane/openagenda-api
  * @license     https://opensource.org/licenses/mit-license.php MIT License
  */
-
 namespace OpenAgenda\Test\TestCase\Endpoint;
 
 use Cake\Validation\Validator;
